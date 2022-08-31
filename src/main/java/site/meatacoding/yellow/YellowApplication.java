b755500d4a2e490b14c3479dc8eceb72977dfb2e
yujin9907 <heoyujin07@gmail.com>
2022-08-31 09:23:40 +0900

    옐로우 초기화

---
+++ src/main/java/site/meatacoding/yellow/YellowApplication.java
@@ -0,0 +1,13 @@
+package site.meatacoding.yellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowApplication.class, args);
+	}
+
+}
